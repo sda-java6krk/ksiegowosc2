5a6bdf7afec838e0cc9e801c8eda0dd797a5c9f3
marcincagara@gmail.com <marcincag@gmail.com>
2017-12-14 22:36:31 +0100

    #2 - added unique login and password longer than 3 characters(Accountant)

--- src/pl/sdacademy/controllers/AccountantController.java
+++ src/pl/sdacademy/controllers/AccountantController.java
@@ -1,5 +1,7 @@
 package pl.sdacademy.controllers;
 
+import pl.sdacademy.exceptions.AccountantAlreadyExistException;
+import pl.sdacademy.exceptions.AccountantPasswordIsToShort;
 import pl.sdacademy.models.Accountant;
 import pl.sdacademy.models.AccountantRegistry;
 import pl.sdacademy.views.AccountantView;
@@ -9,13 +11,13 @@ import pl.sdacademy.views.AccountantView;
  */
 public class AccountantController {
 
-    public static void createAccountant(String login, String password){
+    public static void createAccountant(String login, String password) throws AccountantAlreadyExistException,AccountantPasswordIsToShort{
         AccountantRegistry.getInstance().addAccountant(new Accountant(login,password));
     }
 
     public static void removeAccountant(String login){
         AccountantRegistry.getInstance().removeAccountant(login);
-        }
+    }
 
     public static void listAccountant(){
         AccountantView.printAccountant(AccountantRegistry.getInstance().getAccountants());
--- src/pl/sdacademy/views/AccountantView.java
+++ src/pl/sdacademy/views/AccountantView.java
@@ -2,14 +2,16 @@ package pl.sdacademy.views;
 
 import pl.sdacademy.models.Accountant;
 
+
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by marcin on 13.12.2017.
  */
 public class AccountantView {
 
-public  static void printAccountant(List<Accountant> accountantList){
+public  static void printAccountant(Set<Accountant> accountantList){
 
     for(Accountant accountant : accountantList){
         System.out.println("login: " + accountant.getLogin());
